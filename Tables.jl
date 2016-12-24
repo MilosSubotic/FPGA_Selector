4288ed7e3f47b6e72bd53c715cc60d7d460abc77
Milos Subotic <milos.subotic.sm@gmail.com>
2016-12-24 04:40:47 +0100

    Have combinations.

--- Tables.jl
+++ Tables.jl
@@ -7,7 +7,9 @@ module Tables
 	Taro.init()
 	
 	using DataArrays
+	using DataFrames
 	
+	#TODO Make some max i.e. end for range coding, like small x or whatever.
 	function read_and_clean_table(file_name, sheet, range)
 		t = DataArray(Taro.readxl(file_name, sheet, range, header=false));
 		t = t[Bool[!all(isna(t[r, :])) for r in 1:size(t)[1]], :]
@@ -29,16 +31,55 @@ module Tables
 			name = basename(fn)[1:end-length(".xls")]
 						
 			s = read_and_clean_table(fn, "Summary", "A3:N30")
-			devices = s[:, 1][:]
+			devices = s[:, 1][:] # Remove NAs.
+			
 			p = read_and_clean_table(fn, "Pins", "B1:ZZ2")
 			packages = p[!isna(p[:])]
+			# Grouped by compatibility.
+			grouped_packages = []
+			for c in 1:size(p)[2]
+				gp = p[:, c][:]
+				gp = gp[!isna(gp)] # Remove NAs.
+				push!(grouped_packages, gp)
+			end
+			
 			ms = read_and_clean_table(fn, "Memory_Speed", "B1:ZZ2")
 			speed_grades = ms[!isna(ms[:])]
+			
+			pt = read_and_clean_table(fn, "Pins", "B5:ZZ5")
+			pt_per_p = Int(length(pt)/length(grouped_packages))
+			pin_types = pt[1:pt_per_p]
+			
+			combs = read_and_clean_table(fn, "Pins", "B7:ZZ30")
+			
+			# First column is device-package combination.
+			# Others columns are pin count for pin_types, respectively.
+			dev_pack_combs = DataFrame()
+			dev_pack_combs[Symbol("dev-pack")] = []
+			for pt in pin_types
+				dev_pack_combs[Symbol(pt)] = []
+			end
+			for (c, gp) in enumerate(grouped_packages)
+				for (r, d) in enumerate(devices)
+					pc = combs[r, (c-1)*pt_per_p+1:c*pt_per_p]
+					pc[isna(pc)] = 0
+					if sum(pc) > 0
+						for p in gp
+							dp = (d, p)
+							push!(dev_pack_combs, [dp; pc])
+						end
+					end
+				end
+			end
+					
 			tables = Dict(
 				"summary" => s,
 				"devices" => devices,
 				"speed_grades" => speed_grades,
 				"packages" => packages,
+				"grouped_packages" => grouped_packages,
+				"pin_types" => pin_types,
+				"dev_pack_combs" => dev_pack_combs
 			)
 			
 			families[name] = tables
--- find_best_FPGA.jl
+++ find_best_FPGA.jl
@@ -11,6 +11,9 @@ display(families["Artix-7"]["summary"])
 display(families["Artix-7"]["devices"])
 display(families["Artix-7"]["speed_grades"])
 display(families["Artix-7"]["packages"])
+display(families["Artix-7"]["grouped_packages"])
+display(families["Artix-7"]["pin_types"])
+display(families["Artix-7"]["dev_pack_combs"])
 
 ###############################################################################
 
